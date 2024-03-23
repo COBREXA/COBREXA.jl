45685bb02c1a4713068bcb575b2b5cbe925126af
Mirek Kratochvil <exa.exa@gmail.com>
2024-03-23 22:35:42 +0100

    maybeget removed (might be temporary)

--- src/misc/maybe.jl
+++ src/misc/maybe.jl
@@ -17,15 +17,6 @@
 """
 $(TYPEDSIGNATURES)
 
-Helper for getting stuff from dictionaries where keys may be easily missing.
-"""
-maybeget(_::Nothing, _...) = nothing
-maybeget(x, k, ks...) = haskey(x, k) ? maybeget(x[k], ks...) : nothing
-maybeget(x) = x
-
-"""
-$(TYPEDSIGNATURES)
-
 Helper for applying functions to stuff that might be `nothing`.
 """
 maybemap(f, ::Nothing, def = nothing) = def
