ced7548273d075b6d0b7d5d1a82495b59de859cf
Mirek Kratochvil <miroslav.kratochvil@uni.lu>
2024-03-28 11:27:00 +0100

    WIP: sampling demo

---
+++ docs/src/examples/11.sampling.jl
@@ -0,0 +1,41 @@
+
+# Copyright (c) 2021-2024, University of Luxembourg                         #src
+# Copyright (c) 2021-2024, Heinrich-Heine University Duesseldorf            #src
+#                                                                           #src
+# Licensed under the Apache License, Version 2.0 (the "License");           #src
+# you may not use this file except in compliance with the License.          #src
+# You may obtain a copy of the License at                                   #src
+#                                                                           #src
+#     http://www.apache.org/licenses/LICENSE-2.0                            #src
+#                                                                           #src
+# Unless required by applicable law or agreed to in writing, software       #src
+# distributed under the License is distributed on an "AS IS" BASIS,         #src
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  #src
+# See the License for the specific language governing permissions and       #src
+# limitations under the License.                                            #src
+
+# # Flux sampling
+
+using COBREXA
+
+download_model(
+    "http://bigg.ucsd.edu/static/models/e_coli_core.json",
+    "e_coli_core.json",
+    "7bedec10576cfe935b19218dc881f3fb14f890a1871448fc19a9b4ee15b448d8",
+)
+
+import JSONFBCModels, GLPK
+
+model = load_model("e_coli_core.json")
+
+# note here: this needs the optimizer to generate warmup. If you have warmup,
+# you can do without one.
+flux_sample(
+    model,
+    optimizer = GLPK.Optimizer,
+    objective_bound = relative_tolerance_bound(0.99),
+    n_chains = 10,
+    collect_iterations = [100],
+)
+
+@test 21.8 < sum(x.O2t)/length(x.O2t) < 22.0 #src
