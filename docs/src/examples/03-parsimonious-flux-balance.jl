66ba222766497e3ee3af5030d5be3899283c503b
St. Elmo Wilken <stelmozors@gmail.com>
2024-04-18 10:23:59 +0200

    pFBA doc improvements

--- docs/src/examples/03-parsimonious-flux-balance.jl
+++ docs/src/examples/03-parsimonious-flux-balance.jl
@@ -16,10 +16,11 @@
 
 # # Parsimonious flux balance analysis
 
-# We will use [`parsimonious_flux_balance_analysis`](@ref) to find the optimal
-# flux distribution in the *E. coli* "core" model.
-#
-# TODO pFBA citation
+# We will use [`parsimonious_flux_balance_analysis`](@ref)  (pFBA) to find the
+# optimal flux distribution in the *E. coli* "core" model. In essence, pFBA
+# first uses FBA to find an optimal objective to the problem, thereafter the sum
+# of all fluxes are minimized using the L2 norm. The benefit here is that the QP
+# ensures a unique solution to the problem.
 
 using COBREXA
 
@@ -36,31 +37,58 @@ import Clarabel # can solve QPs
 
 model = load_model("e_coli_core.json") # load the model
 
-# Use the convenience function to run standard pFBA on
+# Use the convenience function to run standard pFBA on the model. Since pFBA
+# solves a quadratic program, you will need a solver that can handle that.
 
 solution = parsimonious_flux_balance_analysis(
     model;
-    optimizer = Clarabel.Optimizer,
+    optimizer = Clarabel.Optimizer, # can solve QPs
     settings = [silence],
 )
 
 @test isapprox(solution.objective, 0.873922; atol = TEST_TOLERANCE) #src
+
 @test isapprox( #src
     solution.parsimonious_objective, #src
     11414.211988071253, #src
     atol = QP_TEST_TOLERANCE, #src
 ) #src
+
 @test isapprox( #src
     sum(x^2 for x in values(solution.fluxes)), #src
     solution.parsimonious_objective, #src
     atol = QP_TEST_TOLERANCE, #src
 ) #src
 
-# ## Linear version
-#
-# TODO
+# Sometimes, you might want to use a dedicated LP solver to find the solution to
+# the original FBA problem, and then a dedicated QP solver to minimize the
+# fluxes. This can be achieved by using the kwargs in
+# [`parsimonious_flux_balance_analysis`](@ref).
 
 import GLPK
+
+solution = parsimonious_flux_balance_analysis(
+    model;
+    optimizer = Clarabel.Optimizer, # can only solve LPs
+    settings = [silence],
+    parsimonious_optimizer = Clarabel.Optimizer,
+)
+
+@test isapprox(solution.objective, 0.873922; atol = TEST_TOLERANCE) #src
+
+@test isapprox( #src
+    solution.parsimonious_objective, #src
+    11414.211988071253, #src
+    atol = QP_TEST_TOLERANCE, #src
+) #src
+
+# ## Linear version
+
+# If you do not want to minimize the L2 norm, you can minimize the L1 norm of
+# the fluxes (splitting reversible fluxes into only positive components, as
+# mentioned earlier). We export
+# [`linear_parsimonious_flux_balance_analysis`](@ref) that does this, and
+# functions similarly.
 
 linear_solution =
     linear_parsimonious_flux_balance_analysis(model; optimizer = GLPK.Optimizer)
