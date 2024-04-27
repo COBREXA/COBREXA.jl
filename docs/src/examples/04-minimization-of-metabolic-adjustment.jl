f71879fb9f251b41ac4385991942f2337529440b
Mirek Kratochvil <exa.exa@gmail.com>
2024-04-27 22:24:15 +0200

    clean up the moma doc

--- docs/src/examples/04-minimization-of-metabolic-adjustment.jl
+++ docs/src/examples/04-minimization-of-metabolic-adjustment.jl
@@ -17,8 +17,11 @@
 # # Minimization of metabolic adjustment analysis
 
 # Minimization of metabolic adjustment analysis (MOMA) finds a flux solution
-# that is closest, in an L2 sense, to some reference solution or model. Running
-# this kind of analysis is straightforward using ConstraintTrees.
+# that is closest to some reference solution. This may correspond to realistic
+# adjustment of living organisms to various perturbations, such as gene
+# knockout or environmental stress.
+#
+# To demonstrate, let's use the E. coli model.
 
 using COBREXA
 
@@ -28,122 +31,135 @@ download_model(
     "7bedec10576cfe935b19218dc881f3fb14f890a1871448fc19a9b4ee15b448d8",
 )
 
-import AbstractFBCModels.CanonicalModel as CM
+
+# We shall use both quadratic and linear solvers -- the "closest to some
+# reference solution" typically refers to Euclidean ("L2") distance which
+# requires a QP solver, but Manhattan ("L1") distance is also demonstrated
+# below.
+import Clarabel, GLPK
+
+# Because we will have to perform some perturbation, we import the model in
+# canonical Julia structures:
 import JSONFBCModels
-import Clarabel
-import GLPK
+import AbstractFBCModels.CanonicalModel as CM
+ecoli = load_model("e_coli_core.json", CM.Model)
 
-model = load_model("e_coli_core.json", CM.Model)
+# This will be a good reaction for perturbing:
+ecoli.reactions["CYTBD"]
 
-## Using reference solutions
+# To do the perturbation, we create a model of a strain which has mild issues
+# with running the CYTBD reaction. We use `deepcopy` to completely avoid any
+# reference sharing issues.
+limited_ecoli = deepcopy(ecoli)
+limited_ecoli.reactions["CYTBD"].upper_bound = 10.0
 
-# This is the simplest situation, where you supply a reference solution, and a
-# flux solution that is closest to it is returned.
+# ## Finding parsimonious solutions
 
-reference_fluxes =
-    parsimonious_flux_balance_analysis(
-        model,
-        optimizer = Clarabel.Optimizer,
-        settings = [silence],
-    ).fluxes # get all the fluxes
+# Becuase we are interested in realistic flux distributions, we have to use an
+# analysis method which gives one -- in this case, the parsimonious FBA will do
+# just right. For later comparison, we first get the optimal parsimonious flux
+# distribution in the perturbed model:
 
-modified_model = deepcopy(model) # change model, and avoid reference sharing
-
-modified_model.reactions["CYTBD"]
-
-modified_model.reactions["CYTBD"].upper_bound = 10.0
-
-solution_with_reference_fluxes_l2 = metabolic_adjustment_minimization_analysis( # L2 norm used
-    modified_model,
-    reference_fluxes;
+solution = parsimonious_flux_balance_analysis(
+    limited_ecoli,
     optimizer = Clarabel.Optimizer,
     settings = [silence],
 )
 
-# It is also straightforward to use an L1 norm version of MOMA.
+# Now, how much is the flux going to differ if we assume the bacterium did only
+# minimal adjustment from the previous state with unlimited CYTBD?
 
-solution_with_reference_fluxes_l1 = linear_metabolic_adjustment_minimization_analysis(
-    modified_model,
-    reference_fluxes;
-    optimizer = GLPK.Optimizer,
-    settings = [silence],
-)
-
-@test isapprox(
-    solution_with_reference_fluxes_l1.objective,
-    0.21924874959,
-    atol = TEST_TOLERANCE,
-) #src
-@test isapprox(solution_with_reference_fluxes_l1.fluxes.CYTBD, 10.0, atol = TEST_TOLERANCE) #src
-@test solution_with_reference_fluxes_l1.linear_minimal_adjustment_objective < 305 #src
-
-
-# ## Comparing models
-# It is also possible to input metabolic models directly, saving some coding
-# steps.
-
-solution_l2 = metabolic_adjustment_minimization_analysis(
-    modified_model,
-    model;
+moma_solution = metabolic_adjustment_minimization_analysis(
+    limited_ecoli, # the model to be examined
+    ecoli; # the model that gives the reference flux
     optimizer = Clarabel.Optimizer,
     settings = [silence],
 )
 
-@test isapprox(solution_l2.objective, 0.241497, atol = TEST_TOLERANCE) #src
-@test isapprox(
-    solution_with_reference_fluxes_l2.objective,
-    solution_l2.objective,
-    atol = TEST_TOLERANCE,
-) #src
-@test sqrt(solution_l2.minimal_adjustment_objective) < 71 #src
-@test isapprox(solution_l2.fluxes.CYTBD, 10.0, atol = TEST_TOLERANCE) #src
+@test isapprox(moma_solution.objective, 0.241496699165187, atol = TEST_TOLERANCE) #src
+@test sqrt(moma_solution.minimal_adjustment_objective) < 71 #src
+@test isapprox(moma_solution.fluxes.CYTBD, 10.0, atol = TEST_TOLERANCE) #src
+
+# ## Comparing the results
+#
+# The difference between the naive and minimally-adjusting solutions can be
+# extracted using the constraint tree functionality:
+
+import ConstraintTrees as C
+difference = C.zip(-, solution, moma_solution, Float64)
+
+#
+
+sort(collect(difference.fluxes), by = last)
+
 @test isapprox( #src
-    C.reduce( #src
-        max, #src
-        C.zip(
-            (a, b) -> abs(a - b),
-            solution_l2,
-            solution_with_reference_fluxes_l2,
-            Float64,
-        ), #src
-        init = 0.0, #src
-    ), #src
-    0.0, #src
+    maximum(last.(difference.fluxes) .^ 2), #src
+    438.56051577031155, #src
     atol = TEST_TOLERANCE, #src
 ) #src
 
-# Likewise, the same thing can be done with the L1 norm MOMA
+# ## Using a custom reference flux
+#
+# In certain situations, you might want to examine how the model would adjust
+# from a known reaction flux. You can supply it
+# manually as the second argument (instead of the reference model).
 
-solution_l1 = linear_metabolic_adjustment_minimization_analysis(
-    modified_model,
-    model;
+ref = parsimonious_flux_balance_analysis(
+    ecoli,
+    optimizer = Clarabel.Optimizer,
+    settings = [silence],
+)
+
+ref_closest_solution = metabolic_adjustment_minimization_analysis(
+    limited_ecoli,
+    ref.fluxes;
+    optimizer = Clarabel.Optimizer,
+    settings = [silence],
+)
+
+@test isapprox( #src
+    ref_closest_solution.objective, #src
+    moma_solution.objective, #src
+    atol = TEST_TOLERANCE, #src
+) #src
+@test C.reduce( #src
+    max, #src
+    init = 0, #src
+    C.zip((a, b) -> (a - b)^2, ref_closest_solution, moma_solution, Float64), #src
+) < TEST_TOLERANCE #src
+
+# The flux may even be partial (which is common with measured fluxes):
+
+measured_fluxes =
+    C.Tree{Float64}(:EX_ac_e => 5.0, :EX_o2_e => -2.0, :BIOMASS_Ecoli_core_w_GAM => 0.7)
+
+solution_close_to_measurement = metabolic_adjustment_minimization_analysis(
+    limited_ecoli,
+    measured_fluxes;
+    optimizer = Clarabel.Optimizer,
+    settings = [silence],
+)
+
+@test isapprox(solution_close_to_measurement.objective, 0.272247, atol = TEST_TOLERANCE) #src
+
+# ## Efficient linear-metric MOMA
+#
+# The linear version of MOMA avoids having to use the quadratic optimizer in
+# the process, giving you more optimizer choices and (typically) much better
+# performance. Linear MOMA has the same interface as the quadratic one:
+
+linear_moma_solution = linear_metabolic_adjustment_minimization_analysis(
+    limited_ecoli,
+    ecoli;
     optimizer = GLPK.Optimizer,
     settings = [silence],
 )
 
-@test isapprox(
-    solution_l1.linear_minimal_adjustment_objective,
-    304.409209299351,
-    atol = TEST_TOLERANCE,
-) #src
+sort(collect(linear_moma_solution.fluxes), by = last)
 
-# ## Comparing fluxes
-
-# Since we are using ConstraintTrees, it becomes trivial to compare logical
-# units (e.g. groups of fluxes) between solutions. Here we will compare how
-# different the fluxes are between the L1 and L2 norms. Since ConstraintTrees
-# makes use of functional programming concepts, this is a simple one liner.
-
-import ConstraintTrees as C
+# How much does the flux distribution differ from the L2 solution?
 
 sort(
-    collect(
-        C.zip(
-            -,
-            solution_with_reference_fluxes_l2.fluxes,
-            solution_with_reference_fluxes_l1.fluxes,
-            Float64,
-        ),
-    ),
+    collect(C.zip(-, linear_moma_solution.fluxes, moma_solution.fluxes, Float64)),
     by = last,
 )
